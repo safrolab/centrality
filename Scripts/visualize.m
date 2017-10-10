30a0bd18d9502b351a9e1db9a3d484d6009077ca
Hayato Mwesigwa <hushiji@hushiji.cs.clemson.edu>
2017-10-10 14:54:26 -0400

    updt

--- Scripts/visualize.m
+++ Scripts/visualize.m
@@ -3,7 +3,8 @@
 %ssget('HB/lap_25')%
 %load('HB/lap_25')
 %load('HB/saylr1')
-load('grid10x10.mat')
+%load('grid10x10.mat')
+load('star_path_star2.mat')
 %load('HB/1138_bus')
 %A = Problem.A;
 G = graph(A);
@@ -14,34 +15,21 @@ A = G.adjacency;
 
 [~, n] = size(A);
 
-steps= 4;
-
+steps= 3;
+num_labelled_nodes = 1;
+labelled_nodes = zeros(num_labelled_nodes,1);
+labelled_nodes(1) = 7;
 I = speye(n);
 %A = A - I; %remove loops
 J = sparse(zeros(n));
 
 D = A*ones(n,1);
-
-for i = 1:10
-    v = randi(n)
-    J(v,v) = 1; 
-    %J(sort_order(i),sort_order(i)) = 1;   
+for i=1:num_labelled_nodes
+   indx = labelled_nodes(i);
+   J(indx, indx) = 1;
 end
 
 
-%J(1,1) = 1;
-%J(2,2) =1;
-%J(6,6)= 1;
-%J(3,3) = 1;
-%J(11,11) = 1;
-%J(15,15)= 1;
-%J(23,23)= 1;
-%J(24,24) = 1;
-%J(20,20) = 1;
-%J(25,25) = 1;
-%for i=x+1:n
-%    J(i,i) =0;
-%end
 B = zeros(n*steps, n*steps);
 B = sparse(B);
 for i=0:steps-1
@@ -52,45 +40,39 @@ for i=0:steps-2
     B(i*n+1:(i+1)*n,(i+1)*n +1:(i+2)*n) = (I-J)*A*(I-J);
 end
 B(1:n,n+1:2*n) = A*(I-J);
-%spy(B^10)
-%colormap('default')
-%imagesc(B^100)
-%colorbar
+
 Ink = speye(n*steps,n*steps);
 X = sparse(zeros(n*steps, n));
 Y = sparse(zeros(n*steps, n));
 for i=0:steps-1
     X(i*n+1:(i+1)*n, :) = I;
-    %Y(i*n+1:(i+1)*n, :) = I;
 end
 Y(1:n,:) = I;
-%spy(X'*B*X-A)
-%Z = Y'*B^u*X;
-%spy(Z - A^u)
-%spy(B)
-%spy(Z)
-%clearvars
-a1 = 1/eigs(A,1) - 0.01;
-%a1 = 0.1;
-a2 = 1/eigs(B,1) - 0.01;
-W1  = (I- a1*A)^(-1)*ones(n,1);
-W2 = Y'*(speye(n*steps) - a2*B)^(-1)*X*ones(n,1);
-colormap('jet');
+
+a1 = 1/abs(eigs(A,1)) - 0.01;
+a2 = 1/abs(eigs(B,1)) - 0.01;
+
+init_cen = ones(n,1);
+for i=1:num_labelled_nodes
+   indx = labelled_nodes(i);
+   init_cen(indx) = 2;
+end
+W1  = (I- a1*A)\init_cen;
+W2 = Y'*((speye(n*steps) - a2*B)\(X*ones(n,1)));
+%W1 = W1/norm(W1);
+%W2 = W2/norm(W2);
+%U = [W1';W2'];
+%[newW1, sort_order] = sort(W1);
+%newW2 = W2(sort_order,:);
+
+%plot([1:n]', newW1,'LineWidth',3)
+%hold on
+%scatter([1:n]', newW2, 'filled')
+%hold off
+scatter(W1, W2)
 W1 = W1/norm(W1);
 W2 = W2/norm(W2);
-U = [W1';W2'];
-[newW1, sort_order] = sort(W1);
-newW2 = W2(sort_order,:);
-
-plot([1:n]', newW1,'LineWidth',3)
-hold on
-scatter([1:n]', newW2, 'filled')
-hold off
-%bar([W1,W2])
-%bar([newW1,newW2])
-
-%imagesc(U');
-%colorbar;
-%C = Y'*(B^10001 +B^10001 + B^10002+ B^10003+B^10004)*X;
-%spy(C)
-%clear;
+[katz, katz_order] = sort(W1,'descend');
+[Bound_katz, Bound_katz_order] = sort(W2,'descend');
+[[1:n]', katz_order, Bound_katz_order]
+[[1:n]', W1,W2]
