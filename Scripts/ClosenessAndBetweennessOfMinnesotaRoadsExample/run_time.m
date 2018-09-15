878e33e6539c558ff1a15967aa10f7ef462358d2
Hayato Mwesigwa <hushiji@g.clemson.edu>
2018-09-14 21:59:14 -0400

    delete *.m

--- Scripts/ClosenessAndBetweennessOfMinnesotaRoadsExample/run_time.m
+++
@@ -1,5 +0,0 @@
-load('grid10x10.mat')
-[~,n] = size(A);
-tic
-wcc = RW_centrality_rand_install(A,4,10);
-toc
--- Scripts/matlab/data/node_rank.m
+++
@@ -1,5 +0,0 @@
-function[myrank] = node_rank(v, v_size)
-rank = 1:v_size;
-[~, node_perm] = sort(v, 'descend');
-[~, myrank] = sort(node_perm);
-end
